f213250eb53e2937c7026d38a8204c6065d7f5a8
Lisa <93735521+Kuponiyi@users.noreply.github.com>
2022-12-12 16:54:50 +0200

    Add files via upload

---
+++ number4/file.js
@@ -0,0 +1,10 @@
+//remove the last elements from the array
+ car = ["mercedes", "toyota", "honda", "ferrari"];
+console.log(car.pop())
+
+//ii return the third element of the array
+console.log(car[2])
+
+//iii add a new car at the beggining of the array
+car.unshift("benz")
+console.log(car)
